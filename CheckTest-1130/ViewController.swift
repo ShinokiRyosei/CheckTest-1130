94ef5e93e1c83258542fbe620366dd09bb8de349
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-11-26 06:05:08 +0900

    Fixed code

--- CheckTest-1130/ViewController.swift
+++ CheckTest-1130/ViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var textField: UITextField!
+    
+    let userDefaults: UserDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        textField.delegate = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let str: String = userDefaults.string(forKey: "text") {
+            textField.text = str
+            
+        }
+        textField.becomeFirstResponder()
+    }
+    
+    @IBAction func selectSave(sender: UIButton) {
+        if let str: String = textField.text {
+            userDefaults.set(str, forKey: "text")
+        }
+    }
+    
+    // Close keyboard if return key is tapped.
+    // Returキーでキーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
