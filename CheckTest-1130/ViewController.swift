b0330f46f451f7b624fd8af589389ae3c5af77bc
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-11-26 05:48:19 +0900

    initial commit

---
+++ CheckTest-1130/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CheckTest-1130
+//
+//  Created by ShinokiRyosei on 2016/11/26.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+}
+
---
+++ CheckTest-1130Tests/CheckTest_1130Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CheckTest_1130Tests.swift
+//  CheckTest-1130Tests
+//
+//  Created by ShinokiRyosei on 2016/11/26.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import XCTest
+@testable import CheckTest_1130
+
+class CheckTest_1130Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
